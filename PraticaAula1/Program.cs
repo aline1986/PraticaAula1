b1a94f2d34021acbd5c7a5227a65576e6cd001ad
marcio henrique <mhmuzy@embratel.com.br>
2021-12-29 22:38:29 -0300

    Adicionar arquivos de projeto.

---
+++ PraticaAula1/Program.cs
@@ -0,0 +1,76 @@
+﻿using PraticaAula1.Entities;
+using PraticaAula1.Repositories;
+using System;
+
+
+//Define alocalização da classe dentro do projeto
+namespace PraticaAula1
+{
+    //Definição daclasse
+    class Program
+    {
+        //Método (funções) para executar o projeto
+        static void Main(string[] args)
+
+        {       // variavel    Inicializando variável para acessar os atributos(propriedades) da classe no caso funcionario 
+            var funcionario = new Funcionario();
+
+            //acessando propriedades da classe = atribuindo valor nas propriedades{set}
+            funcionario.IdFuncionario =  Guid.NewGuid();
+
+            Console.WriteLine("\n***Exerício 01-WebDeveloper****\n ");
+
+            Console.Write("Informe o nome do funcionário:");
+            funcionario.Nome = Console.ReadLine();   //ler o que usuario preencheu e depois de dar enter
+
+            Console.Write("Informe o nome do Cpf do funcionário:");
+            funcionario.Cpf = Console.ReadLine();
+
+            Console.Write("Informe a matrícula do funcionário:");
+            funcionario.Matricula = Console.ReadLine();
+
+            Console.Write("Informe salário do funcionário:"); 
+            funcionario.Salario = decimal.Parse(Console.ReadLine());
+
+
+            Console.Write("Informe data de admissão do funcionário:");
+            DateTime dateTime = DateTime.Parse(Console.ReadLine());
+            funcionario.DataAdmissao = dateTime;
+
+            
+            Console.Write("Informe cargo do funcionário:");
+            funcionario.Cargo = Console.ReadLine();
+
+            //imprimindo dados do cliente no console{get}
+            Console.WriteLine("\nDADOS DO FUNCIONÁRIO:\n"); //\n Quebrar linha
+            Console.WriteLine("IdFuncionário...:"+ funcionario.IdFuncionario); //fazer um tab
+            Console.WriteLine("NOME...:" + funcionario.Nome );
+            Console.WriteLine("CPF..:" + funcionario.Cpf);
+            Console.WriteLine("MATRICULA...:" + funcionario.Matricula);
+            Console.WriteLine("SALARIO...:"+ funcionario.Salario);
+            Console.WriteLine("CARGO...:" + funcionario.Cargo);
+            Console.WriteLine("DATA DA ADMISSÃO...:" + funcionario.DataAdmissao);
+
+            
+            try //tentativa de fazer algo
+            {
+                //Criando variável de instância
+                var funcionarioRepository = new FuncionarioRepository();  //Chamar o codigo
+           
+            //executando a gravação do arquivo
+            
+            funcionarioRepository.ExportarDados(funcionario);
+
+                Console.WriteLine("\nDADOS GRAVADOS COM SUCESSO!!");
+
+            }
+            catch (Exception e)  //Caputurar erro e traçar plano b
+            {
+                Console.WriteLine("\nOcorreu um erro!!! ");
+                
+            }
+               //pausa a execução do prompt
+               Console.ReadKey();
+        }        
+    }
+}
---
+++ PraticaAula1/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,37 @@
+﻿using PraticaAula1.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//localização da classe no projeto
+namespace PraticaAula1.Repositories
+{ 
+    //definição da classe
+    public class FuncionarioRepository
+    {    
+        //Método para gravar os dados do cliente em um arquivo
+        public void ExportarDados(Funcionario funcionario)
+        {
+            //Abrir arquivo em modo de escrita
+            var streamWriter = new StreamWriter("c:\\temp\\funcionario.txt");
+
+            //escrevendo os dados do cliente no arquivo
+            Console.WriteLine("IdFuncionário...:" + funcionario.IdFuncionario); //fazer um tab
+            Console.WriteLine("NOME...:" + funcionario.Nome);
+            Console.WriteLine("CPF..:" + funcionario.Cpf);
+            Console.WriteLine("MATRICULA...:" + funcionario.Matricula);
+            Console.WriteLine("SALARIO...:" + funcionario.Salario);
+            Console.WriteLine("CARGO...:" + funcionario.Cargo);
+            Console.WriteLine("DATA DA ADMISSÃO...:" + funcionario.DataAdmissao);
+            Console.WriteLine("........................");
+
+            //Salvando e fechando o arquivo
+            streamWriter.Flush();
+            streamWriter.Close();
+
+        }
+    }
+}
